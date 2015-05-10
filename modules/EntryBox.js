a5221a7f020daac36b6ffe07fa51c2ea1e803b58
Dennis Li <dennis.li.92@gmail.com>
2015-05-09 17:00:12 -0700

    Basic isLong

--- modules/EntryBox.js
+++ modules/EntryBox.js
@@ -2,9 +2,46 @@ import React from '../bower_components/react/react-with-addons.js';
 
 let EntryBox = React.createClass({
 
+  statics: {
+    isHaiku(text) {
+    },
+
+    isLong(text) {
+      return text.split(" ").length > 140;
+    }
+  },
+
+  getInitialState() {
+    return {
+      value: "Only haikus and 140 words +",
+      canSubmit: false
+    }
+  },
+
+  handleChange(event) {
+    let newValue = event.target.value;
+    this.setState({
+      value: newValue,
+      canSubmit: EntryBox.isLong(newValue) || EntryBox.isHaiku(newValue)
+    });
+  },
+
+  doSomething() {
+    console.log("Hello world");
+  },
+
   render() { 
-    return <div>Hello world</div>;
+    return (
+      <div>
+        <div>
+          <textarea type="text" value={this.state.value} onChange={this.handleChange} />
+        </div>
+
+        <button type="button" disabled={!this.state.canSubmit}>Moooaaaaannnn</button>
+      </div>
+    );
   }
+
 })
 
-module.exports = EntryBox;
+export default EntryBox;
--- modules/entry.js
+++ modules/entry.js
@@ -2,4 +2,4 @@ import EntryBox from './EntryBox.js';
 import React from '../bower_components/react/react.js';
 import $ from '../bower_components/jquery/dist/jquery.js';
 
-$(document).ready(() => React.render(<EntryBox />, document.getElementById("app")))
+$(document).ready(() => React.render(<EntryBox />, document.body))
