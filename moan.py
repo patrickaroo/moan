b75ab7d04afb3f63dd6eda4e6f09baee1728122c
Dennis Li <dennis.li.92@gmail.com>
2015-05-09 21:20:04 -0700

    Changed API endpoints

--- moan.py
+++ moan.py
@@ -1,7 +1,8 @@
-from flask import Flask, make_response, render_template, request, url_for, redirect, flash, session, jsonify
+from flask import Flask, Response, render_template, request, url_for, redirect, flash, session, jsonify, send_from_directory
 from flask_debugtoolbar import DebugToolbarExtension
 from flask import request
 import requests
+import ast
 from models import session, Moan
 from forms import MoanForm
 
@@ -16,7 +17,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'guest'
 
@@ -29,9 +30,30 @@ def gnu_terry_pratchett(resp):
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    #return "Hello world!"#render_template('index.html', form=form)
-    # return render_template('index.html', moans = session.query(Moan).all())
-    return render_template('index.html', moans = session.query(Moan).all())
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/posts', methods = ['GET'])
+def get_posts():
+    posts = session.query(Moan).all()
+    response = [post.as_dict() for post in posts]
+    return jsonify(results=response)
+
+@app.route('/post/<postId>', methods=['GET'])
+def get_post(postId = None):
+    post = session.query(Moan).get(postId)
+    return jsonify(post.as_dict())
+
+@app.route('/post/', methods=['POST'])
+def make_post():
+    newPost = Moan()
+    data = ast.literal_eval(request.data)
+
+    newPost.text = data['text']
+    newPost.user = data['user']
+    session.add(newPost)
+    session.commit()
+
+    return "I don't know what I'm doing"
 
 @app.route('/test_post/', methods=['POST'])
 def test_post():
@@ -46,19 +68,6 @@ def test_post():
     else:
             return False
     '''
-
-@app.route('/make_post/', methods=['GET', 'POST'])
-def make_post():
-    form = MoanForm(csrf_enabled = False)
-    if form.validate_on_submit():
-        newPost = Moan()
-        newPost.text = form.moan.data
-        newPost.user = form.user.data
-        session.add(newPost)
-        session.commit()
-        return redirect(url_for('index'))
-    return render_template('make_post.html', form = form)
-
 
 if __name__ == "__main__":
     app.run()
--- models.py
+++ models.py
@@ -33,7 +33,8 @@ class Moan(Base):
     def __repr__(self):
         return str(self.__dict__)
 
-
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 if __name__ == "__main__":
     Base.metadata.create_all(db)
